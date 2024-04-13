fe96bb0551667e1e543d3ee06af652a678d0d749
Victor Cavalcanti <victorcmarques.dev@gmail.com>
2024-04-12 23:58:31 -0300

    Add App15

---
+++ app/src/main/java/com/example/app15/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.app15
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+// App15: RelativeLayout
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
